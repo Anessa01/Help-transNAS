1fde0f48854e901f8e13151046355dbb3ea8a397
Anessa01 <1447584368@qq.com>
2022-05-12 10:50:57 +0800

    1

--- test.py
+++ test.py
@@ -1,7 +1,18 @@
+from cProfile import label
+from re import S
 from mindspore import dataset as ds
 from mindspore.mindrecord import FileReader
 
-M = ds.MindDataset('data/transnasbench/index.mindrecord')
-N = FileReader('data/transnasbench/macro.mindrecord')
+M = ds.MindDataset('data/transnasbench/micro.mindrecord')
+MD = M.create_dict_iterator()
+MT = M.create_tuple_iterator()
+
+fulldict = []
+for i in MD:
+    dict = {}
+    s = i['label']
+    dict['dajacency_matrix']=[s[-8] + s[-6] + s[-5] + s[-3] + s[-2] + s[-1]]
+    dict['']
+    print(i['label'])
 
 print(M)
